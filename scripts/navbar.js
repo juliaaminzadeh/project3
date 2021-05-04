5b40ed95dd05478c55fe9ce3fb63b4c2ca9e4b7a
juliaaminzadeh <juliaa3@live.unc.edu>
2021-05-04 14:28:00 -0400

    event listeners

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -72,6 +72,8 @@ function swapImages() {
   imgs[1].classList.toggle("active");
 }
 
+function dropDown() {
+
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -89,19 +91,4 @@ for (i = 0; i < acc.length; i++) {
       panel.style.display = "block";
     }
   });
-}
-
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
+}}
